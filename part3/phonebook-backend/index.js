28b570e0517b2381fd25ffa433e51480ae288349
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-06-24 15:26:30 +0200

    Exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -37,6 +37,18 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = `Person with id of ${id} could not be found`;
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}\n${new Date().toString()}`);
