9d82830e11c1fdd40119e6b78d36a23bb5f8bdf1
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-07-08 17:38:00 +0200

    Exercise 4.5

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,7 +2,11 @@ const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = blogs =>
+  blogs.reduce((max, blog) => (max.likes < blog.likes ? blog : max), blogs[0])
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
