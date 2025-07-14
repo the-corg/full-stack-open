55f0efcaccf6e8bc5d4a20b132a0b6278a94b017
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-07-14 19:01:10 +0200

    Exercise 4.20

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -7,19 +7,10 @@ blogsRouter.get('/', async (request, response) =>
   response.json(await Blog.find({}).populate('user', { username: 1, name: 1 }))
 );
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-
-  if (authorization && authorization.startsWith('Bearer '))
-    return authorization.replace('Bearer ', '');
-
-  return null;
-};
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id)
     return response.status(401).json({ error: 'token invalid' });
 
