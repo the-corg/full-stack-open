57afa6b9e2e629b17198fb536fed31a1f8c3421e
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-08-16 14:25:01 +0200

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [detailed, setDetailed] = useState(false);
+
+  const toggleDetailed = () => setDetailed(!detailed);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenDetailed = { display: detailed ? '' : 'none' };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={toggleDetailed}>{detailed ? 'hide' : 'show'}</button>
+      </div>
+
+      <div style={showWhenDetailed}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user?.name ?? 'Unknown user'}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
