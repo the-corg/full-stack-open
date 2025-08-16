3e690ab8a022fb83a887c2a7090e357cee480b83
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-08-16 21:37:04 +0200

    Exercises 5.9 and 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -87,13 +87,15 @@ const App = () => {
       <Togglable buttonLabel='create new' ref={createFormRef}>
         <CreateForm addBlog={addBlog} />
       </Togglable>
-      {blogs.map(blog => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          likeBlog={async () => await likeBlog(blog)}
-        />
-      ))}
+      {blogs
+        .toSorted((a, b) => b.likes - a.likes)
+        .map(blog => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            likeBlog={async () => await likeBlog(blog)}
+          />
+        ))}
     </div>
   );
 };
